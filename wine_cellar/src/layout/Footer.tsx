9c1da1e1d7386b7325ba6dec7c9d96294e43d032
Filipe Costa Junqueira <filipecj0@gmail.com>
2026-03-26 21:23:13 -0300

    feat: Inserção do READ.me

--- wine_cellar/src/layout/Footer.tsx
+++ wine_cellar/src/layout/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-dark text-white p-4 mt-auto">
       <address className="mb-0 text-center">
         <strong>Filipe Costa Junqueira</strong><br />
-        Data: 26/03/2026<br />
+        Data: 13/04/2026<br />
         Desenvolvimento de Software WEB — Prof. Alexandre Almeida 
       </address>
     </footer>
